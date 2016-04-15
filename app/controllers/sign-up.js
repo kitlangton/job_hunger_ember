368be6dc2bad625cc87600256328bd2e524ca66c
Kit Langton <kitlangton@gmail.com>
2016-04-14 21:43:05 -0400

    GODDAMMMIT YEAHHH FUCK YEAH IT WORKSgs IT REALLY WORKS

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -4,10 +4,12 @@ const { service }  = Ember.inject;
 
 export default Ember.Controller.extend({
 
+  session: service(),
   store: service(),
+  email: null,
+  password: null,
 
   validEmail: Ember.computed('email', function()  {
-    console.log("RUNNING");
     let email = this.get('email');
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
@@ -17,14 +19,21 @@ export default Ember.Controller.extend({
     signUp() {
       let email = this.get('email');
       let password = this.get('password');
-      let store = this.get('store');
+      // let store = this.get('store');
 
-      let user = store.createRecord('user', {
-        email: email,
-        password: password
+      Ember.$.ajax({
+        url: '/users',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          user: {
+            email: email,
+            password: password
+          }
+        }
+      }).then(data => {
+        this.get('session').authenticate('authenticator:devise', email, password);
       });
-
-      user.save();
     }
   }
 });
--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('sign-up');
   this.route('login');
-  this.route('protected', { path: '/' });
+  // this.route('protected', { path: '/' });
 });
 
 export default Router;
