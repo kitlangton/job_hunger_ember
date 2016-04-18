606af00e414485237a9d39e88c1a44d986da6ded
koziscool <kozjob@yahoo.com>
2016-04-18 12:32:59 -0500

    bugfix

--- app/routes/companies/company.js
+++ app/routes/companies/company.js
@@ -1,4 +1,25 @@
+
+// company.js  (show route/view)
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  sessionAccount: Ember.inject.service(),
+
+  model(params) {
+    console.log('model');
+    console.log(params);
+    return this.store.findRecord('company', params.company_id);
+  },
+
+
+  actions: {
+    // createCompany(model) {
+    //   let currentUser = this.get('sessionAccount.currentUser');
+    //   model.set('user', currentUser);
+    //   model.save();
+    //   this.transitionTo('dashboard');
+    // }
+  }
+
 });
