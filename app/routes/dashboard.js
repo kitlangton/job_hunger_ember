494b51ec2992a3b61bb09826e13d577d33f09253
koziscool <kozjob@yahoo.com>
2016-04-19 15:24:35 -0500

    bugfix

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -6,6 +6,7 @@ export default Ember.Route.extend({
   sessionAccount: Ember.inject.service(),
 
   model() {
+    let currentUser = this.get('sessionAccount.currentUser');
     return this.store.findRecord('user', currentUser.id, {include: 'companies,jobs,leads'});
   }
 
