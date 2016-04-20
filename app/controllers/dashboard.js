1ce8c56b9d0a3466905ec907a36aeb0146a5de1c
Julia Herron <julia.herron@gmail.com>
2016-04-20 15:23:31 -0700

    limit recommendations to 3

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  recommendations: Ember.computed('model.recommendations', function(){
+    return this.get('model.recommendations').slice(0,3)
+  })
 });
