0cb66e22d1dfe4f9f8c335e93f61be8d58453a5e
Kit Langton <kitlangton@gmail.com>
2016-04-20 19:01:59 -0400

    yay

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,15 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   recommendations: Ember.computed('model.recommendations', function(){
-    return this.get('model.recommendations').slice(0,3)
+    return this.get('model.recommendations').filterBy('completed', false).slice(0,3);
   }),
-
-  actions: {
-    goToRecommendable(recommendation) {
-      recommendation.get('recommendable').then((recommendable) => {
-        this.transitionToRoute('recommendable');
-      });
-    }
-  }
-
 });
