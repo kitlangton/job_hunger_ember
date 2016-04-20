5bdc7fdba1afdb685a5c78648526679538b5a879
Thomas Lo <thomasjinlo@gmail.com>
2016-04-20 16:52:30 -0700

    Add input form and button for creation of new resources

--- app/controllers/companies/new.js
+++ app/controllers/companies/new.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+  store: Ember.inject.service(),
+  sessionAccount: Ember.inject.service(),
   isEmpty: Ember.computed.empty('model.name'),
   validInput: Ember.computed.not('isEmpty'),
   isDisabled: Ember.computed.not('validInput'),
 
+  actions: {
+    createCompany(model) {
+      let currentUser = this.get('sessionAccount.currentUser');
+
+      model.set('user', currentUser);
+      model.save();
+
+      this.transitionToRoute('companies.company', model);
+    },
+  }
 });
