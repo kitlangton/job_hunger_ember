b399713269fe9571286d23b764085404a0a0868e
Kit Langton <kitlangton@gmail.com>
2016-04-20 18:18:26 -0400

    Fix leads new and add some styling

--- app/services/background-color.js
+++ app/services/background-color.js
@@ -3,21 +3,19 @@ import Ember from 'ember';
 export default Ember.Service.extend({
 
   setVeryDark() {
+    this.set('state', 'dark');
     Ember.$('html').addClass('very-dark');
   },
 
   setDark() {
+    this.set('state', 'dark');
     Ember.$('html').addClass('dark');
     var nbDrop = 1458;
 
     // function to generate a random number range.
-    function randRange( minNum, maxNum) {
-      return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
-    }
 
+    this.rainDrop();
     // function to generate drops
-    let height = window.innerHeight;
-    var delay = 100;
     // for(var i=1;i<nbDrop;i++) {
     //   var dropLeft = randRange(0,screen.width);
     //   var dropTop = -90;
@@ -44,7 +42,45 @@ export default Ember.Service.extend({
     // }
   },
 
+
+  rainDrop() {
+    let state = this.get('state');
+    if (state !== 'dark') {
+      return;
+    }
+
+    let height = window.innerHeight;
+
+    function randRange( minNum, maxNum) {
+      return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+    }
+
+    var dropLeft = randRange(0,screen.width);
+    var dropTop = -90;
+
+    let drop = Ember.$('<div class="drop"></div>');
+    Ember.$('.rain').append(drop);
+    drop.css({
+      top: dropTop,
+      left: dropLeft
+    });
+    drop.velocity({
+      translateY: height - 0
+    }, {
+      duration: 1000,
+      complete: () => {
+        drop.velocity({
+          scaleY: 0,
+          // opacity: 0
+        }, 50);
+        Ember.run.later(drop, drop.remove, 200);
+      }
+    });
+    Ember.run.later(this, this.rainDrop, 50);
+  },
+
   setLight() {
+    this.set('state', 'light');
     Ember.$('html').removeClass('dark');
     Ember.$('html').removeClass('very-dark');
   }
