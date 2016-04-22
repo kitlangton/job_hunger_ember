36f58e4fed585d961791f446e8048b253b726b04
Thomas Lo <thomasjinlo@gmail.com>
2016-04-22 16:37:05 -0700

    updated date format

--- app/controllers/meetups.js
+++ app/controllers/meetups.js
@@ -9,6 +9,40 @@ export default Ember.Controller.extend({
   zip: '',
   meetups: [],
 
+  convertTimestamp: function(timestamp) {
+    var d = new Date(timestamp),	// Convert the passed timestamp to milliseconds
+      yyyy = d.getFullYear(),
+      mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+      dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+      hh = d.getHours(),
+      h = hh,
+      min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+      ampm = 'AM',
+      time,
+
+      day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][d.getDay()],
+      month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][d.getMonth()];
+
+    if (hh > 12) {
+      h = hh - 12;
+      ampm = 'PM';
+    } else if (hh === 12) {
+      h = 12;
+      ampm = 'PM';
+    } else if (hh == 0) {
+      h = 12;
+    }
+
+    // ie: 2013-02-18, 8:35 AM
+    time = [day + ', ' + month + ' ' + dd,  h + ':' + min + ' ' + ampm];
+
+    return time;
+  },
+
+  convertAddress: {
+
+  },
+
   actions: {
     clickss() {
       console.log(this.get('category_ids'));
@@ -29,13 +63,28 @@ export default Ember.Controller.extend({
       let radius = this.get('radius');
       let categories = this.get('categories');
       let zip = this.get('zip');
+      let timeStamp = this.get('convertTimestamp');
 
       $.ajax({
         url: 'https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=' + zip + '&category=' + categories + '&radius=' + radius + '&page=20&key=707d395716d1d2a7b6d3c6514732343',
         dataType: "jsonp",
         success: function(response) {
           console.log(response);
-          that.set('meetups', response.results);
+          let parsedResponse = [];
+
+          response.results.forEach(function(element){
+            var obj = {
+              group: element.group.name,
+              name: element.name,
+              distance: Math.floor(element.distance),
+              date: timeStamp(element.time)[0],
+              time: timeStamp(element.time)[1],
+              event_url: element.event_url,
+              yes_rsvp_count: element.yes_rsvp_count
+            };
+            parsedResponse.push(obj);
+          });
+          that.set('meetups', parsedResponse);
         }
       });
     }
