c54887967fd8a9dbadb26569fe6c79b1964e6d8e
Julia Herron <julia.herron@gmail.com>
2016-04-25 15:07:31 -0700

    baby steps, can get new dropdown value to controller

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -4,5 +4,13 @@ export default Ember.Controller.extend({
   recommendations: Ember.computed('model.recommendations.@each.completed', function(){
     return this.get('model.recommendations').filterBy('completed', false).slice(0,3);
   }),
-  statuses: ["Interested", "Applied", "Rejected", "Interview", "Offer"]
+
+  statuses: ["Interested", "Applied", "Rejected", "Interview", "Offer"],
+
+  actions: {
+    updateJobStatus(status) {
+      console.log("made it to updateJobStatus")
+      console.log(status)
+    }
+  }
 });
