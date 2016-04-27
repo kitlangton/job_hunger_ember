15c5755b177830c1a1620fe4832b1afaa92ff14c
Kit Langton <kitlangton@gmail.com>
2016-04-27 12:34:29 -0400

    Add links

--- app/components/job-item.js
+++ app/components/job-item.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['dark-ritual', 'job-item'],
   statuses: ["Interested", "Applied", "Rejected", "Interview", "Offer"],
 
   actions: {
