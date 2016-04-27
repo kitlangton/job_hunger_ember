fc8d23f3dc3b47307b02aa90a35d751fbedb2286
koziscool <kozjob@yahoo.com>
2016-04-27 15:35:10 -0500

    get rid of bad tests

--- tests/acceptance/sign-up-test.js
+++ tests/acceptance/sign-up-test.js
@@ -1,13 +1,22 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'job-hunger/tests/helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | sign up');
+import startApp from '../helpers/start-app';
+import { currentSession, authenticateSession, invalidateSession } from 'job-hunger/tests/helpers/ember-simple-auth';
+
+
+moduleForAcceptance('Acceptance | sign up', {
+  beforeEach: function(){
+    authenticateSession( startApp() );
+  },
+});
 
 test('visiting /sign-up', function(assert) {
   visit('/sign-up');
 
   andThen(function() {
     assert.equal(currentURL(), '/sign-up');
+    // assert.equal(currentURL(), '/login');
   });
 });
 
