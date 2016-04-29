9cc5fc47f5a052d5b7b1fef29c71dc135b90bc72
Julia Herron <julia.herron@gmail.com>
2016-04-29 10:52:42 -0700

    added recommendable to jobs

--- app/models/job.js
+++ app/models/job.js
@@ -1,9 +1,10 @@
-import Model from 'ember-data/model';
+import Recommendable from './recommendable';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 
-export default Model.extend({
+export default Recommendable.extend({
   title: attr('string'),
+  name: Ember.computed.alias('title'),
   company: belongsTo('company'),
   application_status: attr('string'),
   url: attr('string')
